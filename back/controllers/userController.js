c3cc79becf85d28bbb3eab849c66fe4b26c97fe2
cardgis <161532830+cardgis@users.noreply.github.com>
2024-10-06 23:27:32 +0200

    Premier commit

---
+++ back/controllers/userController.js
@@ -0,0 +1,16 @@
+const User = require('../models/userModel');
+const {hashPassword} =  require("../utils/bcrypt");
+const {generateToken} = require("../utils/jwt");
+
+exports.addUser = async (req, res) => {
+  try {
+    req.body.password = hashPassword(req.body.password )
+    const user = new User(req.body);
+    await user.save();
+    let accessToken = generateToken(user.id, "access")
+    let refreshToken = generateToken(user.id, "refresh")
+    res.status(200).json({"accessToken": accessToken, "refreshToken": refreshToken});
+  } catch (error) {
+    res.status(400).json({error: error.message});
+  }
+};
---
+++ back/routes/authRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const authController = require('../controllers/authController');
+const userController = require("../controllers/userController");
+
+router.post('/register', userController.addUser);
+router.post('/login', authController.login);
+router.post('/refresh_token', authController.refreshToken);
+router.get("/validate_token", authController.validateToken);
+
+module.exports = router;
